d5f6e290668708c0b077c98b8961f68b30af80e1
mathonrupinder <mathonrupinder97@gmail.com>
2020-09-15 12:36:23 +0530

    UC2: Last Name validation is added

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,9 +1,19 @@
+echo ".....USER REGISTRATION VALIDATION PROGRAM...."
 read -p "Enter first name:" firstName
 pattern1="^([A-Za-z]{3,})*$"
 if [[ $firstName =~ $pattern1 ]]
 then
-      echo "yes"
+      read -p "Enter last name:" lastName
+          pattern2="^([A-Za-z]{3,})*$"
+            if [[ $lastName =~ $pattern2 ]]
+               then
+                     echo "ok"
+            else
+                    echo "enter valid input"
+             fi
 else
       echo  "enter valid input"
 fi
+
+
 
